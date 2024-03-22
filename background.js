6aec0b1de49ec0eb0c1e109899152fc18b3afcd6
Anujesh Ansh <anshanujesh@gmail.com>
2024-03-22 22:07:06 +0530

    Background.JS

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 let intervalId;
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.action === 'start') {
-    if (intervalId) clearInterval(intervalId); // Clear any existing interval
-    intervalId = setInterval(function() {
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        const tab = tabs[0];
-        chrome.tabs.update(tab.id, {url: message.url});
-      });
-    }, message.interval);
-  } else if (message.action === 'stop') {
-    clearInterval(intervalId);
-  }
+    if (message.action === 'start') {
+        if (intervalId) clearInterval(intervalId); // Clear any existing interval
+        intervalId = setInterval(function() {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                const tab = tabs[0];
+                chrome.tabs.update(tab.id, {url: message.url});
+            });
+        }, message.interval);
+    } else if (message.action === 'stop') {
+        clearInterval(intervalId);
+    }
 });
